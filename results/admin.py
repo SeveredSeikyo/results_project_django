0c9e3e4e7da1728782c136722a87ef544ef1066a
severedseikyo <thanmaysadguru5912@gmail.com>
2025-04-25 10:16:43 +0530

    Second Command

--- results/admin.py
+++ results/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Branch,Result
 # Register your models here.
+
+class ResultAdmin(admin.ModelAdmin):
+    list_display=['roll_number','name','percentage','final_result']
+
+admin.site.register(Result,ResultAdmin)
+admin.site.register(Branch)
--- results/models.py
+++ results/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Branch(models.Model):
+    name=models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class Result(models.Model):
+    roll_number=models.CharField(max_length=20)
+    name=models.CharField(max_length=50)
+    branch=models.ForeignKey(Branch,on_delete=models.DO_NOTHING)
+    year=models.CharField(max_length=20)
+    big_data=models.IntegerField(default=0)
+    ml=models.IntegerField(default=0)
+    dppm=models.IntegerField(default=0)
+    stm=models.IntegerField(default=0)
+    atcd=models.IntegerField(default=0)
+    percentage=models.DecimalField(max_digits=5, decimal_places=2)
+    final_result=models.CharField(max_length=4)
