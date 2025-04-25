084a78af13c228e8079312b75a28baf4ef458e21
severedseikyo <thanmaysadguru5912@gmail.com>
2025-04-25 11:44:05 +0530

    Third Commit

--- results/views.py
+++ results/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import Result
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    if request.method=='GET':
+        return render(request,'index.html')
+    if request.method=='POST':
+        roll_number=request.POST.get('roll_number')
+        try:
+            result=Result.objects.get(roll_number=roll_number)
+        except Result.DoesNotExist:
+            error='Invalid Roll Number'
+            context={'error':error}
+            return render(request,'index.html',context)
+        
+        context={'result':result}
+        return render(request,'results.html',context)
